ac832e3a0168a92fe97d293944a329a2340270d1
Junyu Lu <j.lu-17@student.tudelft.nl>
2023-11-18 17:22:16 +0100

    working on spliting models

--- src/decode.rs
+++ src/decode.rs
@@ -1,25 +1,24 @@
-use serde_json::{Result, Value};
+use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::Read;
 
-pub fn json_to_weights(file:File)->Result<()>{
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-
-    // Deserialize the JSON string
-    let restored_list: Vec<String> = serde_json::from_str(&contents)?;
-
-    // Print the restored list
-    println!("{:?}", restored_list);
-
-    Ok(())
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ConvMapping {
+    map_weights: Vec<Vec<Vec<f64>>>,
+    o_i_mapping: String,
 }
-#[cfg(test)]
-mod tests{
-    use super::*;
 
-    #[test]
-    fn test_read(){
+pub fn json_to_weights() {
+    // Read the JSON file
+    let mut file = File::open("serialized_list.json").expect("Unable to open file");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("Unable to read file");
 
+    // Deserialize the JSON data
+    let conv_mapping: Vec<ConvMapping> = serde_json::from_str(&contents).expect("Unable to deserialize JSON");
+    let temp = &conv_mapping[0];
+    // Now you can use the deserialized data as needed
+    for mapping in conv_mapping {
+        println!("{:?}", mapping);
     }
 }
