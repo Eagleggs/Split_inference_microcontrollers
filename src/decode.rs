6513fc4ce7bcac95fdd5f8c18a04e45063823868
Junyu Lu <j.lu-17@student.tudelft.nl>
2023-11-19 16:06:32 +0100

    rewrite json file

--- src/decode.rs
+++ src/decode.rs
@@ -4,28 +4,20 @@ use std::fs::File;
 use std::io::Read;
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct ConvMapping {
-    weights: Vec<Vec<Vec<f64>>>,
-    mapping: OIMapping,
+pub struct Mapping {
+    i: Vec<Vec<i16>>,
+    w: Vec<f64>,
 }
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct OIMapping {
-    i_ch_s: String,
-    s: Vec<usize>,
-    k: Vec<usize>,
-}
-pub fn decode_json()-> HashMap<String,ConvMapping> {
+pub fn decode_json(mut file : File)-> HashMap<String,Mapping> {
     // Read the JSON file into a string
-    let mut file = File::open("serialized_list.json").expect("Failed to open file");
+    // let mut file = File::open("serialized_list.json").expect("Failed to open file");
     let mut json_string = String::new();
     file.read_to_string(&mut json_string)
         .expect("Failed to read file");
 
     // Deserialize the JSON string into a ConvMapping struct
-    let conv_mapping: HashMap<String,ConvMapping> = serde_json::from_str(&json_string).expect("Failed to deserialize JSON");
+    let mapping: HashMap<String,Mapping> = serde_json::from_str(&json_string).expect("Failed to deserialize JSON");
 
     // Now you can access the data in conv_mapping
-    println!("{:?}", conv_mapping);
-    return conv_mapping;
+    return mapping;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-extern crate
-fn main(){
+use std::fs::File;
+use std::io::Read;
+
+mod decode;
+
+pub fn main(){
+    let mut file = File::open("json_files/serialized_list.json").expect("Failed to open file");
+    let result = decode::decode_json(file);
+    // Access the parsed data as needed
+
+    print!("!");
 }
