8b18090437d9a2edbeb04e4ec55b6c6c0ee10783
Junyu Lu <j.lu-17@student.tudelft.nl>
2023-11-20 20:04:16 +0100

    rewrite json file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::Read;
+use crate::decode::Layer;
 
 mod decode;
 
@@ -7,12 +8,13 @@ pub fn main(){
     let mut file = File::open("json_files/test2.json").expect("Failed to open file");
     let result = decode::decode_json(file);
     // Iterate over the entries and print each key-value pair
-    let mut sorted = result.into_iter().collect::<Vec<_>>();
+    let mut sorted = result.into_iter().collect::<Vec<(i16,Box<dyn Layer>)>>();
     sorted.sort_by_key(|&(x,_)|x);
     for (key, value) in sorted.into_iter(){
         println!("Layer: {}", key);
         // Assuming Layer has a debug implementation
-        println!("Value: {:?}", value.identify());
+        println!("Type: {:?}", value.identify());
+        println!("Info: {:?}", value.get_info());
         println!("---");
     }
 
