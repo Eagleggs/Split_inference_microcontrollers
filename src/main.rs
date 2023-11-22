d5110f79f8781cb0835c3d6ea7e49d0aeabbea95
Junyu Lu <j.lu-17@student.tudelft.nl>
2023-11-22 15:22:02 +0100

    working on some tests

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,7 @@ pub fn main() {
 }
 #[cfg(test)]
 mod tests {
+    use std::io::{BufRead, BufReader};
     use super::*;
     use crate::lib::Conv;
 
@@ -41,10 +42,25 @@ mod tests {
         let mut data: Vec<Vec<Vec<f64>>> = Vec::with_capacity(channels);
         for _ in 0..channels {
             let mut channel: Vec<Vec<f64>> = Vec::with_capacity(width);
-            for i in 0..width {
-                channel.push(vec![i as f64; height]);
+            for i in 0..height {
+                channel.push(vec![i as f64; width]);
             }
             data.push(channel);
+        }
+        let file = File::open("json_files/conv.txt").expect("f");
+        let reader = BufReader::new(file);
+
+        // Initialize a Vec<f64> to store the values
+        let mut reference: Vec<f64> = Vec::new();
+
+        // Iterate over the lines and parse each line as a f64
+        for line in reader.lines() {
+            let line = line.expect("line read failed");
+            if let Ok(value) = line.trim().parse::<f64>() {
+                reference.push(value);
+            } else {
+                eprintln!("Error parsing line: {}", line);
+            }
         }
         for i in 0..output_shape[0] {
             for j in 0..output_shape[1] {
@@ -53,7 +69,8 @@ mod tests {
                     let inputs_p = r.get_input(pos);
                     let mut weights: Vec<f64> = r.get_weights_from_input(inputs_p.clone(), i);
                     let inputs = util::get_input_from_p_zero_padding(inputs_p, &data);
-                    calculations::vector_mul_b(inputs, weights, 0.);
+                    let result = calculations::vector_mul_b(inputs, weights, 0.);
+                    assert_eq!(result,reference[(i * output_shape[1] * output_shape[2] + j * output_shape[2] + m) as usize])
                 }
             }
         }
