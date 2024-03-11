c56108dc55acf7074616dc6e5469f52a86e57866
Junyu Lu <j.lu-17@student.tudelft.nl>
2024-03-11 20:53:14 +0100

    working on quantization

--- Quantization/src/lib/quant.rs
+++ Quantization/src/lib/quant.rs
@@ -10,10 +10,10 @@ use std::io::{BufRead, BufReader};
 //r = (q-z) * s; https://arxiv.org/abs/1712.05877v1
 pub fn quantize_layers_weights(
     layers: &HashMap<i32, Box<dyn Layer>>,
-) -> (Vec<Vec<u8>>, Vec<f32>, Vec<u8>) {
+) -> (Vec<Vec<u8>>, Vec<f32>, Vec<f32>) {
     let mut res = Vec::new();
     let mut scales = vec![0.;100];
-    let mut zero_points = vec![0;100];
+    let mut zero_points = vec![0.;100];
     //determine the float point range
     for i in 1..=layers.len() {
         let l = layers.get(&(i as i32));
@@ -38,7 +38,7 @@ pub fn quantize_layers_weights(
             .unwrap();
         let range = weights_max - weights_min;
         let scale = range / 255.;
-        let zero_point = -(weights_min / scale).round() as u8; // z = -r / s + q
+        let zero_point = -(weights_min / scale); // z = -r / s + q
         let mut weights_quantized = layer
             .get_weights()
             .into_iter()
@@ -252,7 +252,7 @@ pub fn quantize_layers_activation(
         zero_points.into_iter().map(|x| x.round() as u8).collect(),
     )
 }
-pub fn calculate_quantization(original_weights: Vec<Vec<WeightUnit>>,original_mapping:Vec<Mapping>,weight_scales : Vec<f32>,weight_zero_points : Vec<u8>,layer_id:usize)->(Vec<Vec<QuantizedWeightUnit>>,Vec<QuantizedMapping>) {
+pub fn calculate_quantization(original_weights: Vec<Vec<WeightUnit>>,original_mapping:Vec<Mapping>,weight_scales : Vec<f32>,weight_zero_points : Vec<f32>,layer_id:usize)->(Vec<Vec<QuantizedWeightUnit>>,Vec<QuantizedMapping>) {
     //pre calculated values using quantize_layers_activation function
     let scales:Vec<f32> = vec![0.0, 0.017818455, 0.017857317, 0.010181317, 0.07693508, 0.023514507, 0.041469865, 0.04173334, 0.019598939, 0.040599767, 0.016114194, 0.026925236, 0.01976499, 0.0057206596, 0.022290565, 0.011403129, 0.040337086, 0.020162629, 0.0084821, 0.022076631, 0.009201978, 0.021326661, 0.008397463, 0.0041799145, 0.015718713, 0.006351808, 0.026975844, 0.0077458634, 0.004276918, 0.013787641, 0.0064519304, 0.03300309, 0.012218302, 0.0061818487, 0.013625881, 0.007596627, 0.017282467, 0.0056892363, 0.0030426302, 0.010880587, 0.0047031413, 0.019367196, 0.005499944, 0.0027530068, 0.011284228, 0.0051408643, 0.020728296, 0.006147768, 0.002869781, 0.012964951, 0.00812551, 0.022537425, 0.0075556524, 0.004070429, 0.014004531, 0.007840167, 0.016347256, 0.008830495, 0.0041988418, 0.018410122, 0.00890296, 0.019542953, 0.010620198, 0.0057104784, 0.018760901, 0.008726812, 0.026837224, 0.010573086, 0.0054687196, 0.012701936, 0.008290729, 0.013501433, 0.00947222, 0.005058123, 0.018148044, 0.0073697055, 0.019419255, 0.008566049, 0.0048613106, 0.017253218, 0.0076185213, 0.0346372, 0.009283503, 0.0035971922, 0.010591191, 0.004404244, 0.0094860755, 0.069449075, 0.023512602, 0.09354488, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
     let zero_points : Vec<f32> = vec![0.0, 114.38545, 109.03897, 0.0, 108.3703, 0.0, 129.86067, 133.89502, 0.0, 153.77016, 0.0, 117.14856, 179.86311, 0.0, 123.245834, 0.0, 120.96629, 147.66423, 0.0, 147.94286, 0.0, 136.40172, 127.67802, 0.0, 150.21545, 0.0, 132.43462, 114.412674, 0.0, 135.89473, 0.0, 134.72113, 126.012344, 0.0, 112.941055, 0.0, 132.12167, 118.3496, 0.0, 144.18782, 0.0, 130.06107, 127.58742, 0.0, 137.86534, 0.0, 130.81909, 136.1324, 0.0, 103.3621, 0.0, 131.53062, 117.90426, 0.0, 112.85589, 0.0, 125.70349, 134.3532, 0.0, 127.507, 0.0, 126.38503, 123.73302, 0.0, 136.63579, 0.0, 124.32738, 126.83683, 0.0, 91.00306, 0.0, 133.01697, 122.20514, 0.0, 152.75891, 0.0, 126.05996, 112.76912, 0.0, 138.11069, 0.0, 125.67789, 129.37666, 0.0, 143.43307, 0.0, 127.76359, 112.98336, 0.0, 88.16121, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
@@ -269,7 +269,7 @@ pub fn calculate_quantization(original_weights: Vec<Vec<WeightUnit>>,original_ma
     m = s1 * s2 / s3;
     let mut zero1 = zero_points[layer_id].round() as u8;
     let zero2  = weight_zero_points[layer_id];
-    if zero2 == 0 { panic!("weights not get") }
+    if zero2 == 0. { panic!("weights not get") }
     let mut zero3 = zero_points[layer_id + 1].round() as u8;
     if zero_points[layer_id] == 0. {
         zero1 = zero_points[layer_id - 1].round() as u8;
@@ -280,14 +280,14 @@ pub fn calculate_quantization(original_weights: Vec<Vec<WeightUnit>>,original_ma
     let quant_weights = original_weights.into_iter().map(|x|{
             x.into_iter().map(|y|{
                 QuantizedWeightUnit{
-                    data: y.data.into_iter().map(|i| ((i / s2).round() as u8).saturating_add(zero2)).collect(),
+                    data: y.data.into_iter().map(|i| (i / s2 + zero2).round().clamp(0.,255.) as u8).collect(),
                     bias: (y.bias / (s1 * s2)).round() as i32,
                     which_kernel: y.which_kernel,
                     count: y.count,
                     start_pos_in: y.start_pos_in,
                     info: y.info,
                     m: m,
-                    zero_points: (zero1, zero2, zero3),
+                    zero_points: (zero1, zero2.round() as u8, zero3),
                 }
             }).collect::<Vec<QuantizedWeightUnit>>()
         }).collect::<Vec<Vec<QuantizedWeightUnit>>>();
