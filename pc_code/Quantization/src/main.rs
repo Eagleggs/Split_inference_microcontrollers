bc3208b473f06cffb1a3a12e74b14519c4d8bb14
Junyu Lu <j.lu-17@student.tudelft.nl>
2024-07-31 21:02:57 +0800

    update

--- pc_code/Quantization/src/main.rs
+++ pc_code/Quantization/src/main.rs
@@ -4,12 +4,14 @@ use quant::quant::{quantize_layers_activation, quantize_layers_weights};
 use std::fs::File;
 
 pub fn main() {
-    let file = File::open(r"C:\Users\Lu JunYu\CLionProjects\Split_learning_microcontrollers_\Fused\fused_layers_141.json").expect("Failed to open file");
+    let file = File::open(r"..\Fused\fused_layers_141.json").expect("Failed to open file");
     let original_layers = decode::decode_json(file);
+    // quantization of weights
     let (res, scale, zero) = quantize_layers_weights(&original_layers);
+    // quantization of activations
     quantize_layers_activation(
         original_layers,
-        r"C:\Users\Lu JunYu\CLionProjects\Split_learning_microcontrollers_\Algorithms\images\test"
+        r"..\Algorithms\images\test"
             .to_string(),
     );
     // merge_batchnorm(original_layers);
